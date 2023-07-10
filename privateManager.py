0388f3e2ee3325c6371383ddbc041116960c3b9a
Kritias <50093609+ElPlaguister@users.noreply.github.com>
2023-07-10 15:51:52 +0900

    INIT

---
+++ privateManager.py
@@ -0,0 +1,12 @@
+
+PATH = './private/key.properties'
+
+def get_key(query):
+    with open(PATH, 'r') as f:
+        d = {}
+        for line in f.readlines():
+            [k, v] = line.split('=')
+            d[k] = v
+    if(query not in d.keys()):
+        return "ERROR: invalid query!"
+    return d[query]
