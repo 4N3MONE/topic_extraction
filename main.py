2d5b2fd2c6672b18fd5a0210c1a1d2c7f6d7d7fb
Kritias <50093609+ElPlaguister@users.noreply.github.com>
2023-07-17 13:10:15 +0900

    Refactor: Main and ChatGPT

--- main.py
+++ main.py
@@ -7,6 +7,7 @@ import pickle
 from tqdm import tqdm
 
 chatgpt = ChatGPT(get_key('openai'))
+log = open('./log.txt', 'w')
 
 def load_data(path='./data/kullm.json'):
     import json
@@ -14,48 +15,27 @@ def load_data(path='./data/kullm.json'):
         data = json.load(f)
     return data
 
-def extract_topic(start_index, interval, log):
-    error_message = None
-    result = []
-    try:
-        instructions = data[start_index : start_index + interval]
-        texts = chatgpt.request_to_chatgpt(instructions)#[-interval:]
-        log.write(f'\ninstructions[{start_index} : {start_index + interval}]\n' + '\n'.join(map(lambda x : x['instruction'], instructions)) + '\n\n')
-        log.write('\n'.join(texts) + '\n')
-        for text in texts :
-            result.append(text.split(':')[-1].replace('"',''))
-        if len(result)!= interval: 
-            return None, f"The length of result is not matched to interval, the length of result is {len(result)} but interval is {interval}"
-    except Exception as e:
-        error_message = e
-    
-    return result, error_message
-
-
-log = open('./log.txt', 'w')
 data = load_data()
-interval = 1
-error_limit = 5
+ERROR_LIMIT = 5
 topics = []
-for start_index in tqdm(range(0,50, interval)):
+for idx in tqdm(range(0,50)):
     error_count = 0
     while(True):
         try:
-            topic, error = extract_topic(start_index, interval, log)
-            if error == None:
-                topics.extend(topic)
-                is_comped = True
-                break
-        except:
-            pass
-        error_count += 1
-        time.sleep(0.5)
-        log.write(f'ERROR in {start_index}: {error}\n')
-        if error_count >= error_limit:
-            log.write(f'ERROR count exceed {error_limit}: {start_index}\n')
-            log.close()
-            exit()
-print('finished.')
+            output = chatgpt.request.to_chatgpt(data[idx])
+            topics.append(output)
+            log.write(f'instruction[{idx}]: {data[idx]}\n')
+            log.write(f'output[{idx}]: {output}\n\n')
+            break
+        except Exception as error:
+            error_count += 1
+            time.sleep(0.5)
+            log.write(f'ERROR in {idx}: {error}\n')
+            if error_count == ERROR_LIMIT:
+                log.write(f'ERROR count exceed {ERROR_LIMIT}: {idx}\n')
+                log.close()
+                exit()
+print('successfully finished.')
 log.close()
 
 
--- src/chatgpt_handler.py
+++ src/chatgpt_handler.py
@@ -1,35 +1,24 @@
 import openai
-import re
 
 class ChatGPT:
     def __init__(self, key, prompt_path = "./prompt.txt"):
         self.prompt_path = prompt_path
         openai.api_key = key
 
-    def encode_prompt(self, prompt_instructions):
+    def encode_prompt(self, prompt_instruction):
         """Encode multiple prompt instructions into a single string."""
         prompt = open(self.prompt_path).read() + "\n"
-
-        for task_dict in prompt_instructions:
-            query = '\"'+task_dict['instruction']+'\"' #+ '\"'+ task_dict['input'] + '\"'
-            prompt += f"'Input': {query}\n"
-        prompt+="Topic:"
+        prompt += f"'Input': \"{prompt_instruction['instruction']}\"\nTopic:"
         return prompt
 
-    def request_to_chatgpt(self, instructions):
-        prompt = self.encode_prompt(instructions)
+    def request_to_chatgpt(self, instruction, return_all=False):
+        prompt = self.encode_prompt(instruction)
         chat_response = openai.ChatCompletion.create(
         model="gpt-3.5-turbo-16k",
         messages=[
             {"role": "system", "content": "당신은 도움이 필요한 어시스턴트입니다."},
-            {"role": "user", "content": prompt},
+            {"role": "user", "content": prompt}
         ]
         )
-        topics = chat_response['choices'][0]['message']['content'].split('\n')
-<<<<<<< HEAD
-        return topics
-    
-    
-=======
-        return topics
->>>>>>> a880eb2 (Delete: demo.py)
+        topics = chat_response['choices'][0]['message']['content']
+        return chat_response if return_all else topics
