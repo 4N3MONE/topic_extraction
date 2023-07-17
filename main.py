06e44d9ba5d873d0556a034d8ac78d574a9cc0cd
Kritias <50093609+ElPlaguister@users.noreply.github.com>
2023-07-17 13:24:04 +0900

    Feat: Topic Save

--- main.py
+++ main.py
@@ -1,47 +1,46 @@
 from privateManager import get_key
-import openai
 from src.chatgpt_handler import ChatGPT
 import time
-import pandas as pd
-import pickle
 from tqdm import tqdm
+import json
 
-chatgpt = ChatGPT(get_key('openai'))
-log = open('./log.txt', 'w')
+ERROR_LIMIT = 5
+SLEEP_TIME = 0.5
 
 def load_data(path='./data/kullm.json'):
-    import json
     with open(path, 'r') as f:
         data = json.load(f)
     return data
 
-data = load_data()
-ERROR_LIMIT = 5
-topics = []
-for idx in tqdm(range(0, 50)):
-    error_count = 0
-    while(True):
-        try:
-            output = chatgpt.request.to_chatgpt(data[idx])
-            topics.append(output)
-            log.write(f'instruction[{idx}]: {data[idx]}\n')
-            log.write(f'output[{idx}]: {output}\n\n')
-            break
-        except Exception as error:
-            error_count += 1
-            time.sleep(0.5)
-            log.write(f'ERROR in {idx}: {error}\n')
-            if error_count == ERROR_LIMIT:
-                log.write(f'ERROR count exceed {ERROR_LIMIT}: {idx}\n')
-                log.close()
-                exit()
-print('successfully finished.')
-log.close()
+def save_data(obj, path='./data/result_01.json'):
+    with open(path, 'w') as f:
+        json.dump(obj, f)
 
-
-with open('result.p', 'wb') as f:
-    pickle.dump(topics, f)
-print('pickle saved.')
-df = pd.DataFrame(topics)
-df.to_csv('result.csv',encoding='utf-8')
-print('csv saved.')
+if __name__=="__main__":
+    chatgpt = ChatGPT(get_key('openai'))
+    data = load_data()
+    log = open('./log.txt', 'w')
+    
+    start_index = 0
+    end_index = len(data)
+    
+    for idx in tqdm(range(start_index, end_index)):
+        error_count = 0
+        while(True):
+            try:
+                data[idx]['topic'] = chatgpt.request.to_chatgpt(data[idx])
+                log.write(f'instruction[{idx}]: {data[idx]["instruction"]}\n')
+                log.write(f'output[{idx}]: {data[idx]["topic"]}\n\n')
+                break
+            except Exception as error:
+                error_count += 1
+                time.sleep(SLEEP_TIME)
+                log.write(f'ERROR in {idx}: {error}\n')
+                if error_count == ERROR_LIMIT:
+                    log.write(f'ERROR count exceed {ERROR_LIMIT}: {idx}\n')
+                    log.close()
+                    exit()
+    print(f'successfully finished: [{start_index}:{end_index}]')
+    log.close()
+    
+    save_data(data)
