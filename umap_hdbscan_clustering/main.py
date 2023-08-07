03faf8aff04f896a042ee1477bde67ae029792dc
minalang <73943090+minalang@users.noreply.github.com>
2023-08-07 13:22:09 +0900

    Update main.py

--- umap_hdbscan_clustering/main.py
+++ umap_hdbscan_clustering/main.py
@@ -43,10 +43,40 @@ if __name__ == "__main__":
 
     # topic과 이에 대한 kosimCSE 임베딩값으로 구성된 데이터프레임 출력
     embed_df = pd.DataFrame(embed_list, index=topic_list, columns=range(768))
-    embed_df.to_csv('./output/embedding_df.csv', encoding = 'utf-8-sig')
+    embed_df.to_csv('./output_new/embedding_df.csv', encoding = 'utf-8-sig')
     
-    #umap으로 차원축소한 데이터프레임과 시각화결과 저장
-    umap_df = umap_reducer(neighbors = 5, min_distance = 0.1, reduce_dim = 2, data = embed_df)
+    min_dist = [0.1, 0.2, 0.3, 0.4, 0.5]
+    below_cluster_list = []
+    below_data_list = []
+    file_name = []
     
-    #hdbscan을 통해 군집화를 수행한 데이터프레임과 시각화결과 저장
-    fin_df = hdbscan_cluster(min_size = 2, min_sample = None, cluster_distance = 0., data = umap_df)
+    for i in range(3, 7):
+        for j in min_dist:
+            for k in range(2, 5):
+                umap_df = umap_reducer(neighbors = i, min_distance = j, reduce_dim = k, data = embed_df)
+                umap_df.to_csv('./output_new/umap_'+str(i)+'n_'+str(j)+'dist'+str(k)+'dim.csv')
+                fin_df = hdbscan_cluster(min_size = 2, min_sample = None, cluster_distance = 0.01, data = umap_df)
+                
+                #실루엣 스코어 계산 및 미달 군집과 데이터개수 계산
+                X=fin_df.iloc[:,:-1]
+                labels=fin_df.iloc[:,-1]
+                score_samples=silhouette_samples(X, labels)
+                fin_df['silhouette_coeff']=score_samples
+
+                cluster_scores = fin_df.groupby('hdbscan_label')['silhouette_coeff'].mean()
+                cluster_below = cluster_scores[cluster_scores < 0.25].index
+                data_filtered = fin_df[~fin_df['hdbscan_label'].isin(cluster_below)]
+                
+                file_name.append('cluster_'+str(i)+'n_'+str(j)+'dist'+str(k)+'dim.csv')  
+                below_cluster_list.append(len(cluster_below))
+                below_data_list.append(len(fin_df) - len(data_filtered))
+                fin_df.to_csv('./output_new/cluster_'+str(i)+'n_'+str(j)+'dist'+str(k)+'dim.csv')
+                
+                print(f"n{i}, dist{j}, {k}dim 조건 실루엣 스코어가 0.25 미만인 군집 개수: {len(cluster_below)}\n")
+                print(f"n{i}, dist{j}, {k}dim 조건 미달 군집에 포함되는 총 데이터 개수: {len(fin_df) - len(data_filtered)}\n")
+    
+    data = {'파일명': file_name,
+        '미달 군집 수': below_cluster_list,
+        '미달 데이터 수':below_data_list}
+    df = pd.DataFrame(data)
+    df.to_csv('./summary.csv')
